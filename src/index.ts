90612a34ca1f3b82d4ee51ec0b4947b058e61bb4
Sergey Slipchenko <faergeek@gmail.com>
2022-11-16 22:20:50 +0400

    fix: always use push event

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,22 @@
 import { getInput, setFailed } from '@actions/core';
 import { context, getOctokit } from '@actions/github';
 import { HttpClient } from '@actions/http-client';
-import { CreateEvent, PushEvent } from '@octokit/webhooks-definitions/schema';
+import { PushEvent } from '@octokit/webhooks-definitions/schema';
 
 import { parseIssuesFromCommitMessages } from './utils';
 
-async function getAllIssues({
-  base,
-  head,
-  octokit,
-}: {
-  base: string;
-  head: string;
-  octokit: ReturnType<typeof getOctokit>;
-}) {
+const octokit = getOctokit(getInput('github-token', { required: true }));
+const { owner, repo } = context.repo;
+
+async function getAllIssuesSince(base: string) {
   const commitMessages = await octokit.paginate(
     octokit.rest.repos.compareCommits,
     {
       base,
-      head,
-      owner: context.repo.owner,
+      head: context.sha,
+      owner,
       per_page: 100,
-      repo: context.repo.repo,
+      repo,
     },
     response => response.data.commits.map(({ commit }) => commit.message)
   );
@@ -31,76 +26,59 @@ async function getAllIssues({
 
 async function run() {
   try {
-    const octokit = getOctokit(getInput('github-token', { required: true }));
+    if (context.eventName !== 'push') {
+      throw new Error('Only running on push event is supported for now');
+    }
+
+    if (!process.env.GITHUB_REF_NAME) {
+      throw new Error('Expected GITHUB_REF_NAME to be present');
+    }
+
     const hooksUrl = getInput('hooks-url', { required: true });
     const http = new HttpClient();
 
-    switch (context.eventName) {
-      case 'create': {
-        const component = getInput('component', { required: true });
-        const hookRelease = getInput('hook-release', { required: true });
-
-        const payload = context.payload as CreateEvent;
-
-        if (payload.ref_type === 'branch') {
-          throw new Error('Did not expect to be used on branch creation');
-        }
-
-        const [currentTagRef, tags] = await Promise.all([
-          octokit.rest.git.getRef({
-            owner: context.repo.owner,
-            ref: context.ref,
-            repo: context.repo.repo,
-          }),
-          octokit.rest.repos.listTags({
-            owner: context.repo.owner,
-            repo: context.repo.repo,
-            per_page: 1,
-          }),
-        ]);
-
-        const [currentTag, issues] = await Promise.all([
-          octokit.rest.git.getTag({
-            owner: context.repo.owner,
-            repo: context.repo.repo,
-            tag_sha: currentTagRef.data.object.sha,
-          }),
-          getAllIssues({
-            base: tags.data[0].commit.sha,
-            head: currentTagRef.data.object.sha,
-            octokit,
-          }),
-        ]);
-
-        const releaseVersion = currentTag.data.tag;
-        const url = `${hooksUrl}/${hookRelease}`;
-        const body = JSON.stringify({ component, issues, releaseVersion });
-        const headers = { 'Content-Type': 'application/json' };
-
-        await http.post(url, body, headers);
-        break;
-      }
-
-      case 'push': {
+    switch (process.env.GITHUB_REF_TYPE) {
+      case 'branch': {
         const hookMerge = getInput('hook-merge', { required: true });
         const payload = context.payload as PushEvent;
 
-        const issues = await getAllIssues({
-          base: payload.before,
-          head: payload.after,
-          octokit,
-        });
+        const issues = await getAllIssuesSince(payload.before);
 
         const url = `${hooksUrl}/${hookMerge}`;
-        const body = JSON.stringify({ issues });
         const headers = { 'Content-Type': 'application/json' };
+        const body = JSON.stringify({ issues });
 
+        // eslint-disable-next-line no-console
+        console.log(`Reporting merge of issues: ${issues.join(', ')}`);
         await http.post(url, body, headers);
         break;
       }
+      case 'tag': {
+        const component = getInput('component', { required: true });
+        const hookRelease = getInput('hook-release', { required: true });
 
+        const tags = await octokit.rest.repos.listTags({
+          owner,
+          repo,
+          per_page: 1,
+        });
+
+        const issues = await getAllIssuesSince(tags.data[0].name);
+        const releaseVersion = process.env.GITHUB_REF_NAME;
+
+        const url = `${hooksUrl}/${hookRelease}`;
+        const headers = { 'Content-Type': 'application/json' };
+        const body = JSON.stringify({ component, issues, releaseVersion });
+
+        // eslint-disable-next-line no-console
+        console.log(`Reporting release of issues: ${issues.join(', ')}`);
+        await http.post(url, body, headers);
+        break;
+      }
       default:
-        throw new Error(`Unexpected event name: ${context.eventName}`);
+        throw new Error(
+          `Unexpted value of GITHUB_REF_TYPE: ${process.env.GITHUB_REF_TYPE}`
+        );
     }
   } catch (error) {
     setFailed(error instanceof Error ? error.message : String(error));
